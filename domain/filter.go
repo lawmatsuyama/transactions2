cae8ae9ddc369037f979799d472612aa31b6c71e
Lawrence Matsuyama <law.matsuyama@outlook.com>
2022-12-14 15:48:35 -0300

    chore: add filter domain

---
+++ domain/filter.go
@@ -0,0 +1,17 @@
+package domain
+
+import "time"
+
+type AccountFilter struct {
+	ID             string         `json:"id"`
+	DocumentNumber DocumentNumber `json:"document_number"`
+	CreatedAtFrom  time.Time      `json:"created_at_from"`
+	CreatedAtTo    time.Time      `json:"created_at_to"`
+}
+
+func (filter AccountFilter) IsValid() error {
+	if filter.DocumentNumber != "" {
+		return filter.DocumentNumber.IsValid()
+	}
+	return nil
+}
