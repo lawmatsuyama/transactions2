0483e6bd4a64cf0bcf289ac72e81e295a7c425f5
Lawrence Matsuyama <law.matsuyama@outlook.com>
2022-12-14 15:48:18 -0300

    chore: change account domain to add more fields and setters method

--- domain/account.go
+++ domain/account.go
@@ -3,14 +3,29 @@ package domain
 import "time"
 
 type Account struct {
-	ID        DocumentNumber `json:"id" bson:"_id"`
-	CreatedAt time.Time      `json:"created_at" bson:"created_at"`
+	ID             string         `json:"account_id" bson:"_id"`
+	DocumentNumber DocumentNumber `json:"document_number" bson:"document_number"`
+	CreatedAt      time.Time      `json:"created_at" bson:"created_at"`
+}
+
+func NewAccount(docNum DocumentNumber) Account {
+	return Account{DocumentNumber: docNum}
 }
 
 func (acc Account) IsValid() error {
-	if err := acc.ID.IsValid(); err != nil {
+	if err := acc.DocumentNumber.IsValid(); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (acc *Account) SetID() {
+	if acc.ID == "" {
+		acc.ID = UUID.Generate()
+	}
+}
+
+func (acc *Account) SetCreatedAt() {
+	acc.CreatedAt = Now()
 }
